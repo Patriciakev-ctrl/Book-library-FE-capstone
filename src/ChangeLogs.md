6b95528217e4d5a5111842199e472bd568816ce1
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-12-20 20:34:31 +0100

    feat: integrate tailwind css

--- src/ChangeLogs.md
+++
@@ -1,3 +0,0 @@
-# Explanation
-## 1- First pull request from dev branch to main branch
- In the dev branch, i create the **components folder** and the **BookDetails.jsx file**.
---
+++ src/components/BookCards.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function BookCards () {
+
+    return(
+        <div>
+            <p className="text-blue-500">Hello 2025</p>
+        </div>
+    );
+    
+}
+export default BookCards;
